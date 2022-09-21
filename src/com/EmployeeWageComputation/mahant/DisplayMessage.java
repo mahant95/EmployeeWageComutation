2eb5bdf4cb228f12203f355defbf99108f5d0141
mahant95 <montyempire95@gmail.com>
2022-09-21 20:20:00 +0530

    Added Emplyoee Wage Computation Program

---
+++ src/com/EmployeeWageComputation/mahant/DisplayMessage.java
@@ -0,0 +1,9 @@
+package com.EmployeeWageComputation.mahant;
+
+public class DisplayMessage {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation Program");
+}
+}
