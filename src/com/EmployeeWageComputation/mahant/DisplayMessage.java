16df24f3cfe31e9a80777626fbc3cb5b3ebd7f50
mahant95 <montyempire95@gmail.com>
2022-09-21 20:26:54 +0530

    Added Emplyoee Present or Absent

--- src/com/EmployeeWageComputation/mahant/DisplayMessage.java
+++ src/com/EmployeeWageComputation/mahant/DisplayMessage.java
@@ -1,9 +1,19 @@
 package com.EmployeeWageComputation.mahant;
+import java.util.*;
 
 public class DisplayMessage {
 	
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
-}
+
+		Random random = new Random();
+		
+		int randomNumber = random.nextInt(2); // will give 0 or 1 random number
+		
+		if (randomNumber == 1) // condition for present or absent
+			System.out.println("EMPLOYEE IS PRESENT");
+		else
+			System.out.println("EMPLOYEE IS ABSENT");
+	}
 }
