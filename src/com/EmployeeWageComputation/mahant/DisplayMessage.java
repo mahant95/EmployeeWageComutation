e31241462aeeebeab863e15f3f6e1137a7152205
mahant95 <montyempire95@gmail.com>
2022-09-21 20:43:42 +0530

    Added Part-Time-Emp-Wage

--- src/com/EmployeeWageComputation/mahant/DisplayMessage.java
+++ src/com/EmployeeWageComputation/mahant/DisplayMessage.java
@@ -7,18 +7,26 @@ public class DisplayMessage {
 
 		int fullDayHours = 8;
 		int wagePerHours = 20;
+		int halfdayHours = 4;
 		int salary = 0;
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
 
-		int randomNumber = random.nextInt(2); // will give 0 or 1 random number
+		int randomNumber = random.nextInt(3); // will give 0 or 1 random number
 
 		if (randomNumber == 1) { // condition for present or absent
 			System.out.println("EMPLOYEE IS PRESENT");
 			salary = fullDayHours * wagePerHours; // if employee is present than his salary will be
-		} else
+
+		} else if (randomNumber == 2) // condition for half day
+
+		{
+			salary = wagePerHours * halfdayHours;
+		}
+
+		else
 			System.out.println("EMPLOYEE IS ABSENT");
 		System.out.println("Total salary of employee is : " + salary);
 
